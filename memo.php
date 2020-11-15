1f7d541fbb603f270c96d823143016bda6a8af84
kimkim2951 <ty53.k.k@gmail.com>
2020-11-15 18:09:08 +0900

    [add]memo削除機能追加

--- memo.php
+++ memo.php
@@ -30,11 +30,13 @@ $memos = $db->prepare('SELECT * FROM memos WHERE id=?');
 $memos->execute(array($_REQUEST['id']));
 $memo = $memos->fetch();
 ?>
-<article>
-  <pre><?php print($memo['memo']); ?></pre>
-  <a href="update.php?id=<?php print($memo['id']); ?>">編集する</a>
-    ｜
-  <a href="index.php">戻る</a>
+ <article>
+   <pre><?php print($memo['memo']); ?></pre>
+   <a href="update.php?id=<?php print($memo['id']); ?>">編集する</a>
+   |
+   <a href="delete.php?id=<?php print($memo['id']); ?>">削除する</a>
+   |
+   <a href="index.php">戻る</a>
 </article>
 
 </main>
